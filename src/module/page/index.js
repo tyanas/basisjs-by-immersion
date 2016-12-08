0c62715f318754a33810241f81000422e52b713f
tyanas <tyanas.go@gmail.com>
2016-12-08 13:06:10 +0300

    get content as datasource

--- src/module/page/index.js
+++ src/module/page/index.js
@@ -1,6 +1,9 @@
 var Node = require('basis.ui').Node;
 var tabs = basis.require('basis.ui.tabs');
 var router = require('basis.router');
+var Dataset = basis.require('basis.data').Dataset;
+var DataObject = basis.require('basis.data').Object;
+var Value = require('basis.data').Value;
 
 // borrowed from http://basisjs.com/basisjs/demo/defile/tabs.html
 
@@ -15,20 +18,45 @@ var data = [
     { data: { title: 'tab7' }, groupId: 3, childNodes: [{ template: 'content 7' }] }
 ];
 
+var data2 = data.map(function(item) {
+    return new DataObject({
+        data: {
+            selected: item.selected,
+            disabled: item.disabled,
+            title: item.data.title,
+            groupId: item.groupId,
+            content: item.childNodes[0].template
+        }
+    })
+});
+
+var dataSource = new Dataset({
+    items: data2
+});
+
+var currentTab = new Value({ value: 'tab3' });
+
 // TabSheetControl
 var content = new tabs.TabSheetControl({
+    dataSource: dataSource,
     childClass: tabs.TabSheet.subclass({
+        init: function() {
+            tabs.TabSheet.prototype.init.call(this);
+            this.disabled = this.data.disabled;
+            this.setChildNodes([{ template: this.data.content }]);
+        },
+        selected: currentTab.compute(function(node, tab) {
+            return node.data.title == tab;
+        }),
         action: {
             select: function() {
                 if (!this.isDisabled()) {
-                    this.select();
-                    console.log(this);
+                    currentTab.set(this.data.title);
                     router.navigate(this.data.title);
                 }
             }
         }
-    }),
-    childNodes: data
+    })
 });
 
 module.exports = new Node({
