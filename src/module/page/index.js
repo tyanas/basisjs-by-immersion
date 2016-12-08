4915d0185052f0a43dfb5d187a88c2ce7e838685
tyanas <tyanas.go@gmail.com>
2016-12-08 12:26:37 +0300

    add instrumenter; do not ignore framework code

--- src/module/page/index.js
+++ src/module/page/index.js
@@ -5,23 +5,33 @@ var tabs = basis.require('basis.ui.tabs');
 
 // Data source
 var data = [
-  { data: { title: 'tab4' }, groupId: 3, childNodes: [{ template: 'content 4' }] },
-  { data: { title: 'tab1' }, groupId: 1, childNodes: [{ template: 'content 1' }] },
-  { data: { title: 'tab6' }, groupId: 3, childNodes: [{ template: 'content 6' }] },
-  { data: { title: 'tab5' }, groupId: 3, childNodes: [{ template: basis.string.repeat('content 5 ', 100) }] },
-  { data: { title: 'tab3' }, groupId: 2, childNodes: [{ template: 'content 3' }], selected: true },
-  { data: { title: 'tab2' }, groupId: 2, childNodes: [{ template: 'content 2' }], disabled: true },
-  { data: { title: 'tab7' }, groupId: 3, childNodes: [{ template: 'content 7' }] }
+    { data: { title: 'tab4' }, groupId: 3, childNodes: [{ template: 'content 4' }] },
+    { data: { title: 'tab1' }, groupId: 1, childNodes: [{ template: 'content 1' }] },
+    { data: { title: 'tab6' }, groupId: 3, childNodes: [{ template: 'content 6' }] },
+    { data: { title: 'tab5' }, groupId: 3, childNodes: [{ template: basis.string.repeat('content 5 ', 100) }] },
+    { data: { title: 'tab3' }, groupId: 2, childNodes: [{ template: 'content 3' }], selected: true },
+    { data: { title: 'tab2' }, groupId: 2, childNodes: [{ template: 'content 2' }], disabled: true },
+    { data: { title: 'tab7' }, groupId: 3, childNodes: [{ template: 'content 7' }] }
 ];
 
 // TabSheetControl
 var content = new tabs.TabSheetControl({
-  childNodes: data
+    childClass: tabs.TabSheet.subclass({
+        action: {
+            select: function() {
+                if (!this.isDisabled()) {
+                    this.select();
+                    console.log(this);
+                }
+            }
+        }
+    }),
+    childNodes: data
 });
 
 module.exports = new Node({
-  template: resource('./template/page.tmpl'),
-  binding: {
-    content: content
-  }
+    template: resource('./template/page.tmpl'),
+    binding: {
+        content: content
+    }
 });
